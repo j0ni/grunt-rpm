d08f73b3eca1d4b16532293d74a34028786ea162
J Irving <j@lollyshouse.ca>
2014-03-12 17:17:50 -0400

    Some tweaks

--- tasks/lib/default-spec-writer.js
+++ tasks/lib/default-spec-writer.js
@@ -1,101 +1,113 @@
+"use strict";
 
 var path = require('path');
 var fs = require('fs');
 
 
 function unixifyPath(filepath) {
-	if (process.platform === 'win32') {
-		return filepath.replace(/\\/g, '/');
-	} else {
-		return filepath;
-	}
+  if (process.platform === 'win32') {
+	return filepath.replace(/\\/g, '/');
+  } else {
+	return filepath;
+  }
 };
 
 function formatTwoDigists(number) {
-	if (number < 10) {
-		return '0' + number;
-	} else {
-		return number;
-	}
+  if (number < 10) {
+	return '0' + number;
+  } else {
+	return number;
+  }
 }
 
 function formatTimestamp(date) {
-	var str = date.getFullYear();
-	str += formatTwoDigists(date.getMonth() + 1);
-	str += formatTwoDigists(date.getDate());
-	str += formatTwoDigists(date.getHours());
-	str += formatTwoDigists(date.getMinutes());
-	str += formatTwoDigists(date.getSeconds());
-	return str;
+  var str = date.getFullYear();
+  str += formatTwoDigists(date.getMonth() + 1);
+  str += formatTwoDigists(date.getDate());
+  str += formatTwoDigists(date.getHours());
+  str += formatTwoDigists(date.getMinutes());
+  str += formatTwoDigists(date.getSeconds());
+  return str;
 }
 
 function optionalValue(value, defaultValue) {
-	defaultValue = defaultValue || '-';
-	return value ? value : defaultValue;
+  defaultValue = defaultValue || '-';
+  return value ? value : defaultValue;
 }
 
 function formatField(label, attr, separator) {
-	separator = separator || ': ';
-	var str = '';
-	
-	if (attr) {
-		str = label + separator + attr + '\n';
-	}
-	return str;
+  separator = separator || ': ';
+  var str = '';
+
+  if (attr) {
+	str = label + separator + attr + '\n';
+  }
+
+  return str;
 }
 
 function formatLabeledList(label, list) {
-	list = list || [];
-	var str = '';
-	
-	for(var i in list) {
-		str += formatField(label, list[i]);
-	}
-	return str;
+  list = list || [];
+  var str = '';
+
+  for (var i in list) {
+	str += formatField(label, list[i]);
+  }
+
+  return str;
 }
 
 function formatFileList(options) {
-	var str = '';
-	
-	if (options.files && options.files.length > 0) {
-		str += '\n%files\n';
-		if (options.defaultFilemode || options.defaultUsername || options.defaultGroupname || options.defaultDirmode) {
-			str += '%defattr(' 
-				+ optionalValue(options.defaultFilemode) + ',' 
-				+ optionalValue(options.defaultUsername) + ',' 
-				+ optionalValue(options.defaultGroupname) + ','
-				+ optionalValue(options.defaultDirmode) + ')\n';
-		}
-		for (var i in options.files) {
-			var rpmFile = options.files[i];
-//			if (!rpmFile.noRecursion) {
-				if (rpmFile.filemode || rpmFile.username || rpmFile.groupname) {
-					str += '%attr(' 
-						+ optionalValue(rpmFile.filemode) + ',' 
-						+ optionalValue(rpmFile.username) + ',' 
-						+ optionalValue(rpmFile.groupname) + ') ';
-				}
-				str += '"' + unixifyPath(path.join(path.sep, rpmFile.dest, rpmFile.path)) + '"\n';
-//			} else {
-//				str += '%dir "' + unixifyPath(path.join(path.sep, rpmFile.dest + rpmFile.path)) + '"\n';
-//			}
-		}
+  var str = '';
+
+  if (options.files && options.files.length > 0) {
+
+	str += '\n%files\n';
+
+	if (options.defaultFilemode || options.defaultUsername || options.defaultGroupname || options.defaultDirmode) {
+	  str += '%defattr('
+		+ optionalValue(options.defaultFilemode) + ','
+		+ optionalValue(options.defaultUsername) + ','
+		+ optionalValue(options.defaultGroupname) + ','
+		+ optionalValue(options.defaultDirmode) + ')\n';
 	}
-	
-	return str;
+
+	for (var i in options.files) {
+      console.log(i);
+	  var rpmFile = options.files[i];
+      //			if (!rpmFile.noRecursion) {
+	  if (rpmFile.filemode || rpmFile.username || rpmFile.groupname) {
+		str += '%attr('
+		  + optionalValue(rpmFile.filemode) + ','
+		  + optionalValue(rpmFile.username) + ','
+		  + optionalValue(rpmFile.groupname) + ') ';
+	  }
+
+	  str += '"' + unixifyPath(path.join(path.sep, rpmFile.dest, rpmFile.path)) + '"\n';
+      //			} else {
+      //				str += '%dir "' + unixifyPath(path.join(path.sep, rpmFile.dest + rpmFile.path)) + '"\n';
+      //			}
+	}
+  }
+
+  return str;
 }
 
 function readScriptlet(label, scriptFile) {
-	var src = '';
-	if (scriptFile) {
-		var options = {
-			encoding: scriptFile.encoding || 'utf8'
-		};
-		var data = fs.readFileSync(scriptFile.src, options);
-		src += label + '\n';
-		src += data.trim() + '\n\n';
-	}
-	return src;
+  var src = '';
+
+  if (scriptFile) {
+	var options = {
+	  encoding: scriptFile.encoding || 'utf8'
+	};
+
+	var data = fs.readFileSync(scriptFile.src, options);
+
+	src += label + '\n';
+	src += data.trim() + '\n\n';
+  }
+
+  return src;
 }
 
 /**
@@ -103,40 +115,42 @@ function readScriptlet(label, scriptFile) {
  * @module exec
  */
 module.exports = function(options, callback) {
-//	grunt.verbose.writeln('Writing SPEC basic section...');
-	
-	// FIXME the following macro definition should not be needed if the --define command line parameter is taken into account by rpmbuild
-	var src = '%define   _topdir ' + path.join(process.cwd(), options.destination) + '\n\n';
-	
-	src += formatField('Name', options.name);
-	src += formatField('Version', options.version);
-	src += 'Release: ';
-	if (options.release) {
-		src += '1\n';
-	} else {
-		src += 'SNAPSHOT' + formatTimestamp(new Date()) + '\n';
-	}
-	
-	src += formatField('URL', options.homepage);
-	src += formatField('Summary', options.summary);
-	src += formatField('License', optionalValue(options.license, options.licenses[0].type));
-	src += formatField('Distribution', options.distribution);
-	src += formatField('Vendor', options.vendor);
-	src += formatField('Group', options.group);
-	src += formatField('Provides', options.provides);
-	src += formatLabeledList('Requires', options.requires);
-	src += formatField('autoprov', 'yes');
-	src += formatField('autoreq', 'yes');
-	src += formatField('\n%description', options.description, '\n');
-	
-//	grunt.verbose.writeln('Writing SPEC files section...');
-	src += formatFileList(options);
-	
-//	grunt.verbose.writeln('Writing SPEC scriptlet section...');
-	src += readScriptlet('\n%pre', options.preInstall);
-	src += readScriptlet('\n%post', options.postInstall);
-	src += readScriptlet('\n%preun', options.preUninstall);
-	src += readScriptlet('\n%postun', options.postUninstall);
-	
-	fs.writeFile(options.specFilepath, src, callback);
+  //	grunt.verbose.writeln('Writing SPEC basic section...');
+
+  // FIXME the following macro definition should not be needed if the
+  // --define command line parameter is taken into account by rpmbuild
+  var src = '%define   _topdir ' + path.join(process.cwd(), options.destination) + '\n\n';
+
+  src += formatField('Name', options.name);
+  src += formatField('Version', options.version);
+  src += 'Release: ';
+
+  if (options.release) {
+	src += '1\n';
+  } else {
+	src += 'SNAPSHOT' + formatTimestamp(new Date()) + '\n';
+  }
+
+  src += formatField('URL', options.homepage);
+  src += formatField('Summary', options.summary);
+  src += formatField('License', optionalValue(options.license, 'UNKNOWN'));
+  src += formatField('Distribution', options.distribution);
+  src += formatField('Vendor', options.vendor);
+  src += formatField('Group', options.group);
+  src += formatField('Provides', options.provides);
+  src += formatLabeledList('Requires', options.requires);
+  src += formatField('autoprov', 'yes');
+  src += formatField('autoreq', 'yes');
+  src += formatField('\n%description', options.description, '\n');
+
+  //	grunt.verbose.writeln('Writing SPEC files section...');
+  src += formatFileList(options);
+
+  //	grunt.verbose.writeln('Writing SPEC scriptlet section...');
+  src += readScriptlet('\n%pre', options.preInstall);
+  src += readScriptlet('\n%post', options.postInstall);
+  src += readScriptlet('\n%preun', options.preUninstall);
+  src += readScriptlet('\n%postun', options.postUninstall);
+
+  fs.writeFile(options.specFilepath, src, callback);
 };
--- tasks/rpm.js
+++ tasks/rpm.js
@@ -21,139 +21,173 @@ var folders = ['BUILD', 'BUILDROOT', 'RPMS', 'SOURCES', 'SRPMS', specFolder];
  * TODO make this asynchronous.
  * @param {Grunt} grunt - the grunt instance.
  * @param {Array} files - list of file mappings already resolved by grunt.
- * @returns {Array} list of file configuration filtered by invalid sources and with additional configuration that may come along with each file mapping.
+ * @returns {Array} list of file configuration filtered by invalid
+ * sources and with additional configuration that may come along with
+ * each file mapping.
  */
 function filterFiles(grunt, files) {
-	var filesToPack = [];
-	
-	files.forEach(function(fileMapping) {
-		// Fail if there is no dest field defined
-		if (!fileMapping.dest) {
-			grunt.warn('No destination folder is defined for source "' + fileMapping.src + '"');
+  var filesToPack = [];
+
+  files.forEach(function (fileMapping) {
+
+	// Fail if there is no dest field defined
+	if (!fileMapping.dest) {
+	  grunt.warn('No destination folder is defined for source "%s"', fileMapping.src);
+	}
+
+	if (fileMapping.relativeTo && !grunt.file.isDir(fileMapping.relativeTo)) {
+	  grunt.warn('The path specified for relativeTo is not a directory "%s"', fileMapping.relativeTo);
+	}
+
+	// Evaluate specified source files to determine what to do in each case
+	fileMapping.src.forEach(function (filepath) {
+
+	  // Warn on invalid source files (if nonull was set).
+	  if (!grunt.file.exists(filepath)) {
+		grunt.log.warn('Source file "' + filepath + '" does not exists');
+	  } else if (grunt.file.isLink(filepath)) {
+		// TODO handle links
+		grunt.log.warn('Source file "' + filepath + '" is a link and it not supported yet');
+	  } else {
+
+		var fileConfig = grunt.util._.omit(fileMapping, 'src', 'orig', 'filter');
+		fileConfig.src = filepath;
+
+		if (fileMapping.directory) {
+		  if (grunt.file.isDir(filepath)) {
+			fileConfig.directory = true;
+		  } else {
+			grunt.log.warn(
+              'The path %s is not a directory but it is marked as such',
+              filepath,
+              fileMapping.relativeTo);
+		  }
 		}
-		if (fileMapping.relativeTo && !grunt.file.isDir(fileMapping.relativeTo)) {
-			grunt.warn('The path specified for relativeTo is not a directory "' + fileMapping.relativeTo + '"');
+
+		if (fileMapping.relativeTo) {
+		  if (grunt.file.doesPathContain(fileMapping.relativeTo, filepath)) {
+			fileConfig.path = path.relative(fileMapping.relativeTo, filepath);
+		  } else {
+			grunt.log.warn(
+              'The path %s is not contained into the relativeTo directory %s',
+              filepath,
+              fileMapping.relativeTo);
+		  }
+		} else {
+		  fileConfig.path = filepath;
 		}
-		
-		// Evaluate specified source files to determine what to do in each case
-		fileMapping.src.forEach(function(filepath) {
-			// Warn on invalid source files (if nonull was set).
-			if (!grunt.file.exists(filepath)) {
-				grunt.log.warn('Source file "' + filepath + '" does not exists');
-			} else if (grunt.file.isLink(filepath)) {
-				// TODO handle links
-				grunt.log.warn('Source file "' + filepath + '" is a link and it not supported yet');
-			} else {
-				
-				var fileConfig = grunt.util._.omit(fileMapping, 'src', 'orig', 'filter');
-				fileConfig.src = filepath;
-				if (fileMapping.directory) {
-					if (grunt.file.isDir(filepath)) {
-						fileConfig.directory = true;
-					} else {
-						grunt.log.warn('The path %s is not a directory but it is marked as such', filepath, fileMapping.relativeTo);
-					}
-				}
-				if (fileMapping.relativeTo) {
-					if (grunt.file.doesPathContain(fileMapping.relativeTo, filepath)) {
-						fileConfig.path = path.relative(fileMapping.relativeTo, filepath);
-					} else {
-						grunt.log.warn('The path %s is not contained into the relativeTo directory %s', filepath, fileMapping.relativeTo);
-					}
-				} else {
-					fileConfig.path = filepath;
-				}
-				filesToPack.push(fileConfig);
-			}
-		});
+
+		filesToPack.push(fileConfig);
+	  }
 	});
-	
-	return filesToPack;
+  });
+
+  return filesToPack;
 }
 
 /**
- * Copy all the selected files to the tmp folder which wikll be the buildroot directory for rpmbuild 
+ * Copy all the selected files to the tmp folder which wikll be the
+ * buildroot directory for rpmbuild
  */
 function copyFilesToPack(grunt, buildPath, filesToPack) {
-	return function(callback) {
-		grunt.util.async.forEach(filesToPack, function(fileConfig, callback) {
-			try {
-				var filepathDest = path.join(buildPath, fileConfig.dest, fileConfig.path);
-				
-				if (grunt.file.isDir(fileConfig.src)) {
-					if (fileConfig.directory) {
-						// Copy a whole folder to the destination directory.
-						grunt.verbose.writeln('Copying folder "' + fileConfig.src + '" to "' + filepathDest + '"');
-						fs.copyRecursive(fileConfig.src, filepathDest, callback);
-					} else {
-						// Create a folder inside the destination directory.
-						grunt.verbose.writeln('Creating folder "' + filepathDest + '"');
-						grunt.file.mkdir(filepathDest);
-						callback();
-					}
-				} else {
-					// Copy a file to the destination directory inside the tmp folder.
-					grunt.verbose.writeln('Copying file "' + fileConfig.src + '" to "' + filepathDest + '"');
-					grunt.file.copy(fileConfig.src, filepathDest);
-					callback();
-				}
-				
-			} catch(e) {
-				callback(e);
-			}
-		}, callback);
-	};
+  return function (callback) {
+
+	grunt.util.async.forEach(filesToPack, function (fileConfig, callback) {
+
+	  try {
+
+		var filepathDest = path.join(buildPath, fileConfig.dest, fileConfig.path);
+
+		if (grunt.file.isDir(fileConfig.src)) {
+		  if (fileConfig.directory) {
+			// Copy a whole folder to the destination directory.
+			grunt.verbose.writeln('Copying folder "%s" to "%s"', fileConfig.src, + filepathDest);
+			fs.copyRecursive(fileConfig.src, filepathDest, callback);
+		  } else {
+			// Create a folder inside the destination directory.
+			grunt.verbose.writeln('Creating folder "%s"', filepathDest);
+			grunt.file.mkdir(filepathDest);
+			callback();
+		  }
+		} else {
+		  // Copy a file to the destination directory inside the tmp folder.
+		  grunt.verbose.writeln('Copying file "%s" to "%s"', fileConfig.src, filepathDest);
+		  grunt.file.copy(fileConfig.src, filepathDest);
+		  callback();
+		}
+
+	  } catch(e) {
+		callback(e);
+	  }
+
+	}, callback);
+
+  };
 }
 
 /**
  * Write the spec file that rpmbuild will read for the rpm details
  */
 function writeSpecFile(grunt, options, filesToPack) {
-	return function(callback) {
-		try {
-			var specPath = path.join(options.destination, specFolder);
-			options.files = filesToPack;
-			var pkg = grunt.file.readJSON('package.json');
-			grunt.util._.defaults(options, pkg);
-			
-			options.specFilepath = path.join(specPath, options.name + '.spec');
-			spec(options, callback);
-		} catch(e) {
-			callback(e);
-		}
-	};
+  return function (callback) {
+
+	try {
+	  var specPath = path.join(options.destination, specFolder);
+	  options.files = filesToPack;
+	  var pkg = grunt.file.readJSON('package.json');
+	  grunt.util._.defaults(options, pkg);
+
+	  options.specFilepath = path.join(specPath, options.name + '.spec');
+	  spec(options, callback);
+	} catch (e) {
+	  callback(e);
+	}
+
+  };
 }
 
 /**
  * Execute rpmbuild as child process passing in the specific details for building this rpm.<br><br>
  * <code>rpmbuild -bb --target noarch --buildroot '/path/to/destination/tmp' --define='_topdir /path/to/destination' '/path/to/destination/SPECS/project.spec'</code>
- * 
+ *
  * @param {Grunt} grunt - the grunt instance.
- * @param {String} buildPath - relative path to the destination folder where rpmbuilder folder structure resides.
+ * @param {String} buildPath - relative path to the destination folder
+ * where rpmbuilder folder structure resides.
  * @param {Object} options - the options object.
- * @returns {Function} async function that executes the callback once it is done.
+ * @returns {Function} async function that executes the callback once
+ * it is done.
  */
 function spawnRpmbuild(grunt, buildPath, options) {
-	return function(callback) {
-		// FIXME make the hard-coded target architecture platform configurable as well as targetVendor and targetOs
-		// --target noarch-redhat-linux
-		var buildroot = path.join(process.cwd(), buildPath);
-//		var topdir = path.join(process.cwd(), options.destination);
-		var specfile = path.join(process.cwd(), options.specFilepath);
-		var target = 'noarch';
-		
-		// FIXME the --define command line parameter is not taken into account by rpmbuild so a macro definition must be specified in the spec file
-		var rpmbuildOptions = {
-			cmd: 'rpmbuild',
-			args: ['-bb', '--target', target, '--buildroot', buildroot, /*'--define=\'_topdir ' + topdir + '\'',*/ specfile],
-		};
-		
-		grunt.log.writeln('Spawning rpmbuild: %j', rpmbuildOptions);
-		grunt.util.spawn(rpmbuildOptions, function(err, result, code) {
-			// TODO check code [and result] for more detailed response
-			callback(err);
-		});
+  return function (callback) {
+	// FIXME make the hard-coded target architecture platform
+	// configurable as well as targetVendor and targetOs --target
+	// noarch-redhat-linux
+	var buildroot = path.join(process.cwd(), buildPath);
+    //		var topdir = path.join(process.cwd(), options.destination);
+	var specfile = path.join(process.cwd(), options.specFilepath);
+	var target = 'noarch';
+
+	// FIXME the --define command line parameter is not taken into
+	// account by rpmbuild so a macro definition must be specified in
+	// the spec file
+	var rpmbuildOptions = {
+	  cmd: 'rpmbuild',
+	  args: [
+        '-bb',
+        '--target', target,
+        '--buildroot', buildroot,
+        /*'--define=\'_topdir ' + topdir + '\'',*/
+        specfile
+      ]
 	};
+
+	grunt.log.writeln('Spawning rpmbuild: %j', rpmbuildOptions);
+
+	grunt.util.spawn(rpmbuildOptions, function (err, result, code) {
+	  // TODO check code [and result] for more detailed response
+	  callback(err);
+	});
+  };
 }
 
 
@@ -162,39 +196,43 @@ function spawnRpmbuild(grunt, buildPath, options) {
  * @returns {Function} function to be called whenever the 'rpm' task is run.
  */
 function createRpmMultiTask(grunt) {
-	return function() {
-		// Tell Grunt this task is asynchronous.
-		var done = this.async();
+  return function () {
+	// Tell Grunt this task is asynchronous.
+	var done = this.async();
 
-		// Merge task-specific and/or target-specific options with these defaults.
-		var options = this.options({
-			destination: 'rpm'
-		});
-		
-		// Create the folder structure needed by rpmbuild.
-		grunt.file['delete'](options.destination);
-		for (var i in folders) {
-			grunt.file.mkdir(path.join(options.destination, folders[i]));
-		}
-		
-		// Compute required information
-		var buildPath = path.join(options.destination, tmpFolder);
-		var filesToPack = filterFiles(grunt, this.files);
-		
-		// Dispatch sequence of tasks to be performed in order to get the rpm
-		grunt.util.async.series([
-			function(callback) {
-				grunt.util.async.parallel([
-					copyFilesToPack(grunt, buildPath, filesToPack),
-					writeSpecFile(grunt, options, filesToPack)
-				], callback);
-			},
-			spawnRpmbuild(grunt, buildPath, options)
-		], done);
-	};
+	// Merge task-specific and/or target-specific options with these defaults.
+	var options = this.options({
+	  destination: 'rpm'
+	});
+
+	// Create the folder structure needed by rpmbuild.
+	grunt.file['delete'](options.destination);
+	for (var i in folders) {
+	  grunt.file.mkdir(path.join(options.destination, folders[i]));
+	}
+
+	// Compute required information
+	var buildPath = path.join(options.destination, tmpFolder);
+	var filesToPack = filterFiles(grunt, this.files);
+
+	// Dispatch sequence of tasks to be performed in order to get the rpm
+	grunt.util.async.series([
+	  function (callback) {
+		grunt.util.async.parallel([
+		  copyFilesToPack(grunt, buildPath, filesToPack),
+		  writeSpecFile(grunt, options, filesToPack)
+		], callback);
+	  },
+	  spawnRpmbuild(grunt, buildPath, options)
+	], done);
+  };
 }
 
-module.exports = function(grunt) {
-	// Please see the Grunt documentation for more information regarding task creation: http://gruntjs.com/creating-tasks
-	grunt.registerMultiTask('rpm', 'Grunt plugin to create RPM packages out of a project.', createRpmMultiTask(grunt));
+module.exports = function (grunt) {
+  // Please see the Grunt documentation for more information regarding
+  // task creation: http://gruntjs.com/creating-tasks
+  grunt.registerMultiTask(
+    'rpm',
+    'Grunt plugin to create RPM packages out of a project.',
+    createRpmMultiTask(grunt));
 };
